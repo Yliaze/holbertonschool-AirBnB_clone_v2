6f6f77c943f66ccb42f1e659c665e05991e65736
Benjamin <benjamin.alazet@gmail.com>
2023-08-03 09:45:38 +0200

    Modify comments

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,4 @@
+-- Creates the database hbnb_test_db and the user hbnb_test
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
