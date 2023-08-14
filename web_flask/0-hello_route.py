a5c96db9a62b75c8784159a16fda75c1bc1ea8a1
Benjamin <benjamin.alazet@gmail.com>
2023-08-14 11:38:35 +0200

    Add documentations

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+"""This script creates a simple Flask web application."""
 from flask import Flask
 
+"""Create an instance of the Flask application"""
 app = Flask(__name__)
 
+"""Decorator that define a route"""
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
     return "Hello HBNB!"
 
+"""Start the Flask application when the script is run directly"""
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
