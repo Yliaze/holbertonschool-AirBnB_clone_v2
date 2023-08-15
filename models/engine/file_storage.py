351f2aac543b2ae92a93dafc34626c5d0a76e699
Benjamin <benjamin.alazet@gmail.com>
2023-08-15 10:00:45 +0200

    Update for task 7 ?

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,13 +10,14 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
+        if cls is not None:
+            filtered_objects = {}
+            for key, obj in FileStorage.__objects.items():
+                if isinstance(obj, cls):
+                    filtered_objects[key] = obj
+            return filtered_objects
+        else:
             return FileStorage.__objects
-        new_dict = {}
-        for k, v in FileStorage.__objects.items():
-            if isinstance(v, cls):
-                new_dict[k] = v
-        return (new_dict)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -51,13 +52,16 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """delete fonction"""
+        """Deletes an object from __objects if it's inside"""
         if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            key = obj.to_dict()['__class__'] + '.' + obj.id
             del FileStorage.__objects[key]
-            FileStorage.save(self)
+    
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
